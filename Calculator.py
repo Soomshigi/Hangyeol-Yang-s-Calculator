23af1d1f45e08a7314eb29f56ffe2c4856d1da63
Soomshigi <92490945+Soomshigi@users.noreply.github.com>
2024-05-11 18:18:14 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+import math
+# import tkinter as tk
+#
+# window = tk.Tk()
+# window.geometry("500x800")
+# window.resizable(0,0)
+# window.title("CALCULATOR")
+# window.mainloop()
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def multi(x,y):
+    return x*y
+def divide(x,y)
+    if y != 0:
+        return x / y
+    else:
+        return 'Error! Division by zero is not allowed.'
+pi = math.pi
